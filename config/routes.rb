8b7c5928f62594d605ded83adbe60505dba19626
Manu Arora <mannuarora700@gmail.com>
2018-09-03 15:33:19 +0530

    Working with the user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   			 path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
   			 controllers: {omniauth_callbacks: 'omniauth_callbacks'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
 end
