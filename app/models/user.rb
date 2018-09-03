8c1c35ff6edccd67ea4e725ebeb4fc9a7158885a
Manu Arora <mannuarora700@gmail.com>
2018-09-03 15:21:02 +0530

    MAJOR STYLING DONE// EVERYTHING LOOKS NICE.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,16 +12,16 @@ class User < ApplicationRecord
   	if user
   		return user
   	else
-		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-		    user.email = auth.info.email
-		    user.password = Devise.friendly_token[0,20]
-		    user.fullname = auth.info.name   
-		    user.image = auth.info.image
-		    user.uid = auth.uid
-		    user.provider = auth.provider 
+			where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+			    user.email = auth.info.email
+			    user.password = Devise.friendly_token[0,20]
+			    user.fullname = auth.info.name   
+			    user.image = auth.info.image
+			    user.uid = auth.uid
+			    user.provider = auth.provider 
 
-		    user.skip_confirmation!
-  		end
-  	end   
+			    user.skip_confirmation!
+	  		end
+  		end   
 	end
 end
